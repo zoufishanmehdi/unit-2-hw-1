76ca848a110cc84c6eb22f9c0b39b3d1b9dbe6fd
mikekavouras <kavourasm@gmail.com>
2015-09-20 20:40:36 -0400

    sorry jamaal and dan

---
+++ TalkinToTheNet/TalkinToTheNet/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TalkinToTheNet
+//
+//  Created by Michael Kavouras on 9/20/15.
+//  Copyright © 2015 Mike Kavouras. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
